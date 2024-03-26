41cedf72f06b761e9f80fab3d002dacde1c8ef9f
Sai Nithesh <134050142+sainithesh147@users.noreply.github.com>
2024-03-26 13:55:42 +0530

    Create Strassen's matrix.c

---
+++ Strassen's matrix.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void strassen(int n, int A[][n], int B[][n], int C[][n]) {
+    int i, j; 
+    if (n == 1) {
+        C[0][0] = A[0][0] * B[0][0];
+        return;
+    }
+
+    int newSize = n / 2;
+    int A11[newSize][newSize], A12[newSize][newSize], A21[newSize][newSize], A22[newSize][newSize];
+    int B11[newSize][newSize], B12[newSize][newSize], B21[newSize][newSize], B22[newSize][newSize];
+    int C11[newSize][newSize], C12[newSize][newSize], C21[newSize][newSize], C22[newSize][newSize];
+    int P1[newSize][newSize], P2[newSize][newSize], P3[newSize][newSize], P4[newSize][newSize], P5[newSize][newSize], P6[newSize][newSize], P7[newSize][newSize];
+    int temp1[newSize][newSize], temp2[newSize][newSize];
+
+    for (i = 0; i < newSize; i++) {
+        for (j = 0; j < newSize; j++) {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][j + newSize];
+            A21[i][j] = A[i + newSize][j];
+            A22[i][j] = A[i + newSize][j + newSize];
+
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][j + newSize];
+            B21[i][j] = B[i + newSize][j];
+            B22[i][j] = B[i + newSize][j + newSize];
+        }
+    }
+
+    strassen(newSize, A11, B11, P1);
+    strassen(newSize, A12, B21, P2);
+    strassen(newSize, A11, B12, P3);
+    strassen(newSize, A12, B22, P4);
+    strassen(newSize, A21, B11, P5);
+    strassen(newSize, A22, B21, P6);
+    strassen(newSize, A21, B12, P7);
+
+    for (i = 0; i < newSize; i++) {
+        for (j = 0; j < newSize; j++) {
+            C11[i][j] = P1[i][j] + P4[i][j] - P5[i][j] + P7[i][j];
+            C12[i][j] = P3[i][j] + P5[i][j];
+            C21[i][j] = P2[i][j] + P4[i][j];
+            C22[i][j] = P1[i][j] - P2[i][j] + P3[i][j] + P6[i][j];
+        }
+    }
+
+    for (i = 0; i < newSize; i++) {
+        for (j = 0; j < newSize; j++) {
+            C[i][j] = C11[i][j];
+            C[i][j + newSize] = C12[i][j];
+            C[i + newSize][j] = C21[i][j];
+            C[i + newSize][j + newSize] = C22[i][j];
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the size of matrices (power of 2): ");
+    scanf("%d", &n);
+
+    int A[n][n], B[n][n], C[n][n];
+    int i, j; 
+
+    printf("Enter elements of matrix A:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of matrix B:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    strassen(n, A, B, C);
+
+    printf("Resultant Matrix C:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
