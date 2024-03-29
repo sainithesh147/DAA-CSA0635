179de99532fe42175083e49373ff436bce122261
Sai Nithesh <134050142+sainithesh147@users.noreply.github.com>
2024-03-29 08:35:18 +0530

    Create daa-22 optimal binary search.c

---
+++ daa-22 optimal binary search.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int optimalSearchTree(int keys[], int freq[], int n) {
+    int cost[n][n];
+
+    for (int i = 0; i < n; i++)
+        cost[i][i] = freq[i];
+
+    for (int L = 2; L <= n; L++) {
+        for (int i = 0; i <= n - L + 1; i++) {
+            int j = i + L - 1;
+            cost[i][j] = INT_MAX;
+			            for (int r = i; r <= j; r++) {
+
+                int c = ((r > i) ? cost[i][r - 1] : 0) +
+                        ((r < j) ? cost[r + 1][j] : 0) +
+                        freq[r];
+                if (c < cost[i][j])
+                    cost[i][j] = c;
+            }
+        }
+    }
+
+    return cost[0][n - 1];
+}
+
+int main() {
+    int keys[] = {10, 12, 20};
+    int freq[] = {34, 8, 50};
+    int n = sizeof(keys) / sizeof(keys[0]);
+
+    printf("Cost of optimal binary search tree is %d\n", optimalSearchTree(keys, freq, n));
+    return 0;
+}
